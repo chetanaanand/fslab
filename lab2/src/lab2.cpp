c485fa9742f16a3a903e40424b26355c5064d893
chetanaanand <anandchetana7@gmail.com>
2016-02-29 14:34:19 +0530

    previous existing program

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -1,3 +1,4 @@
+
 //============================================================================
 // Name        : lab2.cpp
 // Author      : 
@@ -7,9 +8,64 @@
 //============================================================================
 
 #include <iostream>
+#include<fstream>
 using namespace std;
-
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+class student
+{
+string	 usn,name,sem;
+public:
+	void read();
+	void pack();
+	void unpack();
+};
+fstream fp;
+void student::read(void)
+{
+	cout<<"enter the student usn,name,sem"<<endl;
+	cin>>usn;
+	cin>>name;
+	cin>>sem;
+}
+void student::pack(void)
+{
+	string buffer;
+	buffer=usn+"|"+name+"|"+sem;
+	//while(buffer.length()<50)
+    //buffer+='*';
+	buffer.resize(100,'$');
+    fp<<buffer;
+}
+void student::unpack(void)
+{
+	string extra;
+	fp.open("txt.txt",ios::in);
+	getline(fp,usn,'|');
+	cout<<usn;
+	getline(fp,name,'|');
+	cout<<name;
+	getline(fp,sem,'|');
+	getline(fp,extra);
+}
+int main()
+{
+	student s1;
+	int ch,i;
+	for(i=0;i<=3;i++)
+	{
+	cout<<"enter your choice"<<endl;
+	cin>>ch;
+switch(ch)
+{
+case 1:s1.read();
+       fp.open("txt.txt",ios::out);
+       if(!fp)
+       cout<<"not opened";
+       s1.pack();
+       fp.close();
+       break;
+case 2:s1.unpack();
+       break;
+}
+}
+return 0;
 }
